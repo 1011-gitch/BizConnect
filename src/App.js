36ce46df734d3bf5faac31db67497edc57efeb48
DeweshDubey <deweshdubey22@gmail.com>
2022-01-04 21:06:05 +0530

    1st-Png

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Home from './pages/home';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 function App() {
   return (
-   <Router>
-      {/* <Link to="/new">New</Link> */}
-      <Switch>
-        <Route exact path="/" component={Home}/>
-      </Switch>
-   </Router>
+    <>
+    <Home/>
+    </>
   );
 }
 
